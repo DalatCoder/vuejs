f9e2f8b7efdd0c57a7a7c8948f8edc7c54b32dfa
DalatCoder <hieuntctk42@gmail.com>
2021-08-01 14:05:33 +0700

    Complete module 1

--- 01.vue-basics/app.js
+++ 01.vue-basics/app.js
@@ -1,27 +1,102 @@
 const app = Vue.createApp({
   data() {
     return {
-      showName: true,
-      name: 'Trong Hieu',
-      x: 0,
-      y: 0,
+      websiteTitle: 'NTH Book Store',
       books: [
-        { title: 'Book 1', author: 'Author 1' },
-        { title: 'Book 2', author: 'Author 2' },
-        { title: 'Book 3', author: 'Author 3' },
+        {
+          id: 1,
+          title: '199 Mấy Hồi Ấy Làm Gì?',
+          price: '83500',
+          isFav: false,
+          img: 'assets/1.jpg',
+        },
+        {
+          id: 2,
+          title: 'Muôn Kiếp Nhân Sinh 2',
+          price: '186400',
+          isFav: false,
+          img: 'assets/2.jpg',
+        },
+        {
+          id: 3,
+          title: 'Cây Cam Ngọt Của Tôi',
+          price: '74600',
+          isFav: false,
+          img: 'assets/3.jpg',
+        },
+        {
+          id: 4,
+          title: 'Vui Vẻ Không Quạu Nha - Tản Văn',
+          price: '45900',
+          isFav: false,
+          img: 'assets/4.jpg',
+        },
+        {
+          id: 5,
+          title: 'Kiếp Nào Ta Cũng Tìm Thấy Nhau',
+          price: '56000',
+          isFav: false,
+          img: 'assets/5.jpg',
+        },
+        {
+          id: 6,
+          title: 'Rèn Luyện Tư Duy Phản Biện',
+          price: '61200',
+          isFav: false,
+          img: 'assets/6.jpg',
+        },
+        {
+          id: 7,
+          title:
+            'Muốn Thành Công Nói Không Với Trì Hoãn – 21 Nguyên Tắc Vàng Đập Tan Sự Trì Hoãn',
+          price: '62400',
+          isFav: false,
+          img: 'assets/7.jpg',
+        },
+        {
+          id: 8,
+          title: 'Nhà Đầu Tư Thông Minh (Tái Bản 2020)',
+          price: '146.300',
+          isFav: false,
+          img: 'assets/8.jpg',
+        },
+        {
+          id: 9,
+          title: 'Tuổi Trẻ Đáng Giá Bao Nhiêu (Tái Bản)',
+          price: '58700',
+          isFav: false,
+          img: 'assets/9.jpg',
+        },
+        {
+          id: 10,
+          title: 'Mình Chỉ Là Người Bình Thường (Sách Tô Màu)',
+          price: '85700',
+          isFav: false,
+          img: 'assets/10.jpg',
+        },
+      ],
+      menus: [
+        { id: 1, title: 'All', isSelected: true },
+        { id: 2, title: 'Favorites', isSelected: false },
       ],
     }
   },
   methods: {
-    toggleNameDisplay() {
-      this.showName = !this.showName
+    toggleBookFav(book) {
+      book.isFav = !book.isFav
     },
-    handleEvent(event) {
-      console.log(`Event: ${event.type}`)
+    handleMenuClick(menu) {
+      this.menus.forEach((m) => (m.isSelected = false))
+      menu.isSelected = true
     },
-    handleMousemove(event) {
-      this.x = event.offsetX
-      this.y = event.offsetY
+  },
+  computed: {
+    prepareData() {
+      if (this.menus[0].isSelected) {
+        return this.books
+      } else if (this.menus[1].isSelected) {
+        return this.books.filter((b) => b.isFav)
+      }
     },
   },
 })
